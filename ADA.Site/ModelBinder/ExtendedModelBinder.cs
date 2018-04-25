b64fcb63adf7c0c42883ac4b96e07aee4e67b2f9
Benoit <Benoit@AXOM-BENOIT>
2018-04-25 12:33:12 +0200

    retsest

--- ADA.Site/ModelBinder/ExtendedModelBinder.cs
+++ ADA.Site/ModelBinder/ExtendedModelBinder.cs
@@ -12,7 +12,7 @@ using System.Web.Mvc;
 namespace ADA.Site.ModelBinder
 {
     /// <summary>
-    /// Binder permettant le binfing automatique de différents types **********
+    /// Binder permettant le binfing automatique de différents types +++++++++++++
     /// </summary>
     public class ExtendedModelBinder : DefaultModelBinder
     {
